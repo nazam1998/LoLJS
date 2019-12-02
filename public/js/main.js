aaba9f752aeaa0ef255ec63be8bd8fdc30813d1e
Nazam <nazam98-be@email.com>
2019-12-02 16:50:05 +0100

    Without user error

--- public/js/main.js
+++ public/js/main.js
@@ -4,24 +4,27 @@ class Boss {
         this.pV = pV;
         this.pointAtt = pointAtt;
     }
-    poser() {
-        if (this.pv <= (pv * 20 / 100)) {
+    poser(question, sol) {
+        if (this.pV < (this.pV * 20 / 100)) {
             let essai = 3;
 
-            let question = Number(prompt("Que donne Math.round(Math.abs(-50.555556)) ?"));
-            let rep = 51;
-            while (essai > 0 && question != rep) {
+            let rep = prompt(question);
+            while (essai > 0 && !rep.toLowerCase().includes(sol)) {
                 essai--;
                 if (essai > 0) {
-                    let question = Number(prompt("Que donne Math.round(Math.abs(-50.555556)) ?"));
+                    alert("Faux! Recommencez!");
+                    rep =prompt(question);
                 }
             }
-            if (question == rep) {
+            if (rep.includes(sol)) {
                 alert("Bravo ! Vous avez vaincu le boss !");
+                return true
             } else {
                 alert("Dommage ! Vous avez échoué.");
+                return true
             }
         }
+        return false;
     }
 }
 let sauron = new Boss("Sauron", 500, 300);
@@ -44,6 +47,7 @@ let guerrier = {
     rage() {
         rage++;
         if (rage == 4) {
+            alert(`${this.nom} est entré en rage !!!`);
             this.pointAtt *= 1.5;
         }
         if (rage < 4) {
@@ -76,9 +80,9 @@ let mage = {
             this.mana += 7;
             return false;
         }
-        this.mana -= 2;
     }
 }
+
 let nbFleches = [7, 8, 9, 10, 11];
 r = Math.floor(Math.random() * nbFleches.length);
 nbFleches = nbFleches[r];
@@ -100,23 +104,28 @@ let archer = {
             this.fleche = 6;
         } else if (this.fleche == 1) {
             this.fleche += 6;
-        } else {
-            this.fleche++;
         }
-        this.fleche -= 2;
     }
 }
 
 let listBoss = [sauron, chronos, lilith];
+let question = ["Que donne Math.round(Math.abs(-50.555556)) ?", "Quel est le plat préféré de la coding 10 ?", "Quel est la marque de Pc la plus bruyante ?"];
+let sol = ["51", "sushi", "hp"]
 r = Math.floor(Math.random() * listBoss.length);
 let boss = listBoss[r];
 
 alert(`${boss.nom} est le Boss`);
-do {
+// do {
+//     guerrier.nom = prompt("Veuillez nommer le guerrier.");
+//     mage.nom = prompt("Veuillez nommer le mage.");
+//     archer.nom = prompt("Veuillez nommer l'archer.");
+// } while (guerrier.nom.length == 0, mage.nom.length == 0, archer.nom.length == 0)
+
+while (guerrier.nom.length == 0, mage.nom.length == 0, archer.nom.length == 0) {
     guerrier.nom = prompt("Veuillez nommer le guerrier.");
     mage.nom = prompt("Veuillez nommer le mage.");
     archer.nom = prompt("Veuillez nommer l'archer.");
-} while (guerrier.nom.length == 0, mage.nom.length == 0, archer.nom.length == 0)
+}
 
 let totalPv = 300;
 let totalAtt = 300;
@@ -125,33 +134,97 @@ alert("Vous avez 300 points en Pv et 300 en Attaque. Veuillez les distribuer ent
 alert(`${guerrier.nom} le guerrier`);
 
 if (totalPv > 0) {
-    guerrier.pV = prompt(`Combien de points sur ${totalPv} voulez-vous lui assigner en pv?`);
+    guerrier.pV = prompt(`Combien de points sur ${totalPv} voulez-vous lui assigner en PV ? `);
     totalPv -= guerrier.pV;
 }
 if (totalAtt > 0) {
     guerrier.pointAtt = prompt(`Combien de points sur ${totalAtt} voulez-vous lui assigner en attaque?`);
     totalAtt -= guerrier.pointAtt;
 }
-alert(`${guerrier.nom} le guerrier  pv:${guerrier.pV}  pa:${guerrier.pointAtt}`);
+alert(`${guerrier.nom} le guerrier  PV : ${guerrier.pV}  PA : ${guerrier.pointAtt}`);
 alert(`${mage.nom} le mage`);
-alert(`Il vous reste ${totalPv} en pv et ${totalAtt} en attaque`);
+
 if (totalPv > 0) {
-    mage.pV = prompt(`Combien de points sur ${totalPv} voulez-vous lui assigner en pv?`);
+    mage.pV = prompt(`Combien de points sur ${totalPv} voulez-vous lui assigner en PV ? `);
     totalPv -= mage.pV;
 }
 if (totalAtt > 0) {
     mage.pointAtt = prompt(`Combien de points sur ${totalAtt} voulez-vous lui assigner en attaque?`);
     totalAtt -= mage.pointAtt;
 }
-alert(`${mage.nom} le mage  pv:${mage.pV}  pa:${mage.pointAtt} mana:${mage.mana}`);
+alert(`${mage.nom} le mage  PV : ${mage.pV}  PA : ${mage.pointAtt} Mana : ${mage.mana}`);
 alert(`${archer.nom} le archer`);
-alert(`Il vous reste ${totalPv} en pv et ${totalAtt} en attaque`);
 if (totalPv > 0) {
-    archer.pV = prompt(`Combien de points sur ${totalPv} voulez-vous lui assigner en pv?`);
+    archer.pV = prompt(`Combien de points sur ${totalPv} voulez-vous lui assigner en PV ? `);
     totalPv -= archer.pV;
 }
 if (totalAtt > 0) {
     archer.pointAtt = prompt(`Combien de points sur ${totalAtt}. voulez-vous lui assigner en attaque?`);
     totalAtt -= archer.pointAtt;
 }
-alert(`${archer.nom} l'archer  pv:${archer.pV}  pa:${archer.pointAtt} nbFlèches:${archer.fleche}`);
+alert(`${archer.nom} l'archer  PV : ${archer.pV}  PA : ${archer.pointAtt} nbFlèches : ${archer.fleche}`);
+
+alert("Que la bataille commence !");
+
+let tour = 0;
+let listHero = [guerrier, mage, archer];
+let posture = 'normale';
+listHero.forEach(elem => {
+    posture = prompt(`Veuillez choisir un posture parmi celle-ci pour ${elem.nom}: normale/attaque/defense`);
+    if (posture.toLowerCase() == 'attaque') {
+        elem.attaque();
+    } else if (posture.toLowerCase() == 'defense') {
+        elem.defense();
+    }
+});
+do {
+    alert("Tour n°" + tour);
+    alert(`${boss.nom} a ${boss.pV} points de vie`);
+    alert("Tour du Joueur");
+
+
+    if (estVie(guerrier)) {
+        alert(`${guerrier.nom} le guerrier attaque de ${guerrier.pointAtt} points`);
+        boss.pV -= guerrier.pointAtt;
+        alert(`${boss.nom} a ${boss.pV} points de vie`);
+    }
+    if (mage.mana > 2) {
+        alert(`${mage.nom} le mage attaque de ${mage.pointAtt} points`);
+        mage.mana -= 2;
+        boss.pV -= mage.pointAtt;
+    } else {
+        alert(`${mage.nom} le mage n'a pas assez de point de mana`)
+    }
+
+    alert(`${boss.nom} a ${boss.pV} points de vie`);
+    if (archer.fleche > 2) {
+        alert(`${archer.nom} l'archer attaque de ${archer.pointAtt} points`);
+        archer.fleche -= 2;
+        boss.pV -= archer.pointAtt;
+    } else {
+        alert(`${archer.nom} l'archer n'a pas assez de fleche`)
+    }
+
+    alert(`${boss.nom} a ${boss.pV} points de vie`);
+    r = Math.floor(Math.random() * listHero.length);
+    console.log(`${boss.nom} attaque ${listHero[r].nom} de ${boss.pointAtt}`);
+    listHero[r].pV -= boss.pointAtt;
+    console.log(`${listHero[r].nom} a ${listHero[r].pV} points de vie.`);
+
+    tour++;
+} while (boss.poser(question[r].toLowerCase(), sol[r].toLowerCase()) == false && listHero.length > 0);
+if (listHero.length == 0) {
+    alert('Tous les héros sont Ko. Vous avez échouez');
+}
+
+function estVie(hero) {
+    if (hero.pV > 0) {
+        return true;
+    } else {
+        if (listHero.includes(hero)) {
+            listHero.splice(listHero.indexOf(hero), 1);
+            console.log(`${hero.nom} est mort!`);
+        }
+        return false;
+    }
+}
